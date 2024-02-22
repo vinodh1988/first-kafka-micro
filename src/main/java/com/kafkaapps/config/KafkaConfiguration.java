566daf08286274c77a59dd6081762ad3931c2f8c
vinodh1988 <k.c.vinodh@hotmail.com>
2024-02-22 20:34:41 +0530

    first-commit

---
+++ src/main/java/com/kafkaapps/config/KafkaConfiguration.java
@@ -0,0 +1,39 @@
+package com.kafkaapps.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.kafkaapps.entities.Person;
+
+@Configuration
+public class KafkaConfiguration {
+	
+  @Bean 
+  public ProducerFactory<String,Person> producerFactory() {
+	 Map<String,Object> config = new HashMap<>(); 
+	 config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+	 config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+	 config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+			 
+			 
+			
+	 return new DefaultKafkaProducerFactory<>(config);
+	 
+  }
+  
+  @Bean
+  public KafkaTemplate<String,Person> kafkaTemplate() {
+	return new KafkaTemplate<> (producerFactory());  
+  }
+  
+
+}
